9f86f44a2439d6741ea8e84d977b584ead213be2
Anthony Burke <aburke@dreamspike.local>
2017-09-21 22:42:57 +1000

    Seed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,14 @@
-/*
- * Copyright (c) 2016 VMware, Inc. All Rights Reserved.
- * This software is released under MIT license.
- * The full license information can be found in LICENSE in the root directory of this project.
- */
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-    styleUrls: ['./home.component.scss'],
-    templateUrl: './home.component.html',
+  selector: 'home',
+  templateUrl: 'home.component.html',
+  host: {
+    "[class.content-container]": "true"
+  }
 })
 export class HomeComponent {
+
+  constructor() { }
 
 }
